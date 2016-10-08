74b9e91989a307d94efe0ae3ad7a4422d65c8337
domon <domonji95@gmail.com>
2016-10-08 20:00:20 +0800

    client basic components

---
+++ client/src/Blog.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div class="blog-container">
+    <header-nav></header-nav>
+    <div class="blog content">
+      <router-view></router-view>
+    </div>
+    <footer class="footer">
+      Copyrights ©️2016 <a href="#">Domon ji</a>
+    </footer>
+  </div>
+</template>
+
+<script>
+import HeaderNav from './components/HeaderNav'
+export default {
+  components: {
+    HeaderNav
+  }
+}
+</script>
+
+<style lang="css">
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-
+import Blog from './Blog'
+import Router from 'vue-router'
+import routes from './routes'
 /* eslint-disable no-new */
+
+Vue.use(Router)
+const router = new Router()
+routes(router)
+router.start(Blog, 'blog')
 new Vue({
-  el: '#app',
-  render: h => h(App)
+  el: '#blog',
+  render: h => h(Blog)
 })
