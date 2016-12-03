90fd0dc1f8325b7bbad3c469544ecd175da653a9
domon <domonji95@gmail.com>
2016-12-03 22:51:59 +0800

    something

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,16 +2,16 @@ import service from './service.js'
 
 export default {
   getPostList(params) {
-    return service.get('postlist', params)
+    return service.get('posts', params)
   },
   getPost(id) {
-    return service.get(`post/${id}`)
+    return service.get(`posts/${id}`)
   },
   getAllTags() {
     return service.get('tags')
   },
   getPostsByTag(tagId) {
-    return service.get('postlist', {tag: tagId})
+    return service.get('posts', {tag: tagId})
   },
   getAbout() {
     return service.get('about')
